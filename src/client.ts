77eb9dd3de43513293c8c3f594c006031ea842e8
Chris Evardo <chris.evardo@oddball.io>
2020-09-04 10:19:29 -0400

    Add onSuccess and onRejected login messages

--- src/client.ts
+++ src/client.ts
@@ -3,7 +3,13 @@ import { config } from './config'
 
 export const discordClient = async (): Promise<Client> => {
   const client: Client = new Client({ partials: ['MESSAGE', 'REACTION'] }) // Instantiate the Discord Client.
-  await client.login(config.token).catch(error => { console.log(error) })
+  await client.login(config.token)
+    .then(() => {
+      console.log(client.user?.username + ' has logged in.')
+    }, ()=> {
+      console.log(client.user?.username + ' has FAILED logged in.')
+    })
+    .catch(error => { console.log(error) })
 
   return client
 }
