0745b2df4e420d6ae94dfa69dcea964e485ff1b8
Chris Evardo <chris.evardo@oddball.io>
2020-09-03 21:37:08 -0400

    Added dotenv and tested successful token connection

--- src/client.ts
+++ src/client.ts
@@ -2,8 +2,8 @@ import { Client } from 'discord.js'
 import { config } from './config'
 
 export const discordClient = async (): Promise<Client> => {
-	const client: Client = new Client() // Instantiate the Discord Client.
-	await client.login(config.token).catch(error => { console.log(error)})
+  const client: Client = new Client() // Instantiate the Discord Client.
+  await client.login(config.token).catch(error => { console.log(error)})
 
-	return client
+  return client
 }
--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,5 @@
-/**
- * Make a file called config.json and store your token in there!
- */
+require('dotenv').config()
 
 export const config = {
-	'token' : process.env.TOKEN,
-	// 'token': 'NzUwOTM1NTMxMzI3ODQ4NDk4.X1BxNg.ojV6KKB4-TJOe9BPDtYr60ZLctw',
+  'token' : process.env.TOKEN,
 }
