124a815a2f77f59c53f99fb524c94a54c0ce54ed
Chris Evardo <chris.evardo@oddball.io>
2020-09-04 10:21:17 -0400

    Break out message content extraction, fix guild fetching to reduce object ambiguity

--- src/market-bot.ts
+++ src/market-bot.ts
@@ -1,4 +1,4 @@
-import { Client, Guild, Message } from 'discord.js'
+import { Client, Message } from 'discord.js'
 import { discordClient } from './client'
 import { config } from './config'
 
@@ -9,61 +9,61 @@ async function start() {
   const client: Client = await discordClient()
 
   client.once('ready', () => {
-    console.log(client.user?.username + ' has logged in.')
+    console.log(client.user?.username + ' is READY for business.')
   })
 
   client.on('message', async (message: Message) => {
 
-		if (message.author.bot || message.channel.type === 'dm') return
+    if (message.author.bot || message.channel.type === 'dm') return
 
-		const guild = message.guild ? message.guild : new Guild(client, {})
+    const guild = await message.guild!.fetch()
 
     if (message.content.toLowerCase() === '?setup' && guild.ownerID === message.author.id) {
-			try {
-				const filter = (m: Message) => m.author.id === message.author.id
-				message.channel.send('Please enter the message id for this ticket')
-				const msgId: any = (await message.channel.awaitMessages(filter, { max: 1 })).first()!.content
-				const fetchMsg = await message.channel.messages.fetch(msgId)
-				
-				message.channel.send('Please enter the category id for this ticket')
-				const categoryId: any = (await message.channel.awaitMessages(filter, { max: 1 })).first()!.content
-				const categoryChannel = client.channels.cache.get(categoryId)
+      try {
+        message.channel.send('Please enter the message id for this ticket')
+        const msgId = await getFirstMessageContent(message)
+        const fetchMsg = await message.channel.messages.fetch(msgId)
 
-				message.channel.send('Please enter the roles that have access to tickets')
-				const roles = (await message.channel.awaitMessages(filter, { max: 1 })).first()!.content.split(/,\s*/)
+        message.channel.send('Please enter the category id for this ticket')
+        const categoryId = await getFirstMessageContent(message)
+        const categoryChannel = client.channels.cache.get(categoryId)
 
-				if (fetchMsg && categoryChannel ) {
-					for (const roleId of roles) {
-						if (!message.guild!.roles.cache.get(roleId)) throw new Error('Role does not exist!')
-					}
-				}
+        message.channel.send('Please enter the roles that have access to tickets')
+        const roles = await getFirstMessageContent(message)
 
-				// Optionally save config to database
-				
-			} catch (err) {
-				console.log(err)
-			}
-		}
+        if (fetchMsg && categoryChannel) {
+          for (const roleId of roles.split(/,\s*/)) {
+            if (!guild.roles.cache.get(roleId)) throw new Error('Role does not exist!')
+          }
+        }
 
-		/**
+        // Optionally save config to database
+
+      }
+      catch (err) {
+        console.log(err)
+      }
+    }
+
+    /**
 		 * This first conditional statement is used to give reactions to the embed messages our bot sends.
 		 * Please note everything here is hard-coded, you are responsible for modifying it to fit your needs.
 		 */
 
-		if(message.author.bot) {
-			if(message) {
-				if(message.embeds.length === 1 && message.embeds[0]) {
-					message.react(':ticketreact:625925895013662721')
-						.then(() => console.log('Reacted'))
-						.catch(err => console.log(err))
-				}
-				if(message.embeds.length === 1 && message.embeds[0].title === 'Ticket Support') {
-					message.react(':checkreact:625938016510410772')
-						.then(reaction => console.log('Reacted with ' + reaction.emoji.name))
-						.catch(err => console.log(err))
-				}
-			}
-		}
+    if(message.author.bot) {
+      if(message) {
+        if(message.embeds.length === 1 && message.embeds[0]) {
+          message.react(':ticketreact:625925895013662721')
+            .then(() => console.log('Reacted'))
+            .catch(err => console.log(err))
+        }
+        if(message.embeds.length === 1 && message.embeds[0].title === 'Ticket Support') {
+          message.react(':checkreact:625938016510410772')
+            .then(reaction => console.log('Reacted with ' + reaction.emoji.name))
+            .catch(err => console.log(err))
+        }
+      }
+    }
 
 
     if(message.content.toLowerCase() === '?createticket' && message.channel.id === config.marketChannelId) {
@@ -118,31 +118,24 @@ async function start() {
         }
       }
 
-      /**
-			 * Here we will check the server to see if there were additional tickets created that the bot may have missed due to
-			 * either crashing, restarting, etc.. This part will delete ALL of the tickets that follow the format of
-			 * "<username>s-ticket" because that was the way we hard-coded. You can modify this obviously.
-			 */
-			
-			if(guild.channels.)
+      // if(guild.channels.)
 
-      if(guild.channels.some((channel: TextChannel) => channel.name.toLowerCase() === message.author.username + 's-ticket')) {
-        guild.channels.forEach(channel => {
-          if(channel.name.toLowerCase() === message.author.username + 's-ticket') {
-            channel.delete().then(ch => console.log('Deleted Channel ' + ch.id))
-              .catch(err => console.log(err))
-          }
-        })
-      }
+      // if(guild.channels.some((channel: TextChannel) => channel.name.toLowerCase() === message.author.username + 's-ticket')) {
+      //   guild.channels.forEach(channel => {
+      //     if(channel.name.toLowerCase() === message.author.username + 's-ticket') {
+      //       channel.delete().then(ch => console.log('Deleted Channel ' + ch.id))
+      //         .catch(err => console.log(err))
+      //     }
+      //   })
+      // }
     }
   })
 }
 
-async function stripMessages(message: Message) {
-	const filter = (m: Message) => m.author.id === message.author.id
-	const finalMessage = await message.channel.awaitMessages(filter, { max: 1 })
-
-	return finalMessage.first() ? finalMessage.first() : null
+async function getFirstMessageContent(message: Message): Promise<string> {
+  const filter = (m: Message) => m.author.id === message.author.id
+  const finalMessage = (await message.channel.awaitMessages(filter, { max: 1 })).first()!
+  return finalMessage.content
 }
 
 start()
