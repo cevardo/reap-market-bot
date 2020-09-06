b3c1f69c6f6a76347bf54589a28b4a93545a1c85
Chris Evardo <chris.evardo@oddball.io>
2020-09-05 21:16:23 -0400

    Add dbHost

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,8 @@
-require('dotenv').config()
-
 export const config = {
   'token': process.env.TOKEN!,
   'marketChannelId': process.env.MARKET_CHANNEL_ID!,
   'dbName': process.env.DB_NAME!,
   'dbUser': process.env.DB_USER!,
   'dbPassword': process.env.DB_PASS!,
+  'dbHost': process.env.DB_HOST!,
 }
